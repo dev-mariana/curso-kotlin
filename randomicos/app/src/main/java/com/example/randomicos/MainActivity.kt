14584e1d17fd09a216d2c3bf45bcdf59bb9c309c
mariana_bastos <maribastossrodrigues@gmail.com>
2023-02-09 09:59:34 -0300

    add layout app

---
+++ randomicos/app/src/main/java/com/example/randomicos/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.randomicos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val buttonRandom = findViewById<View>(R.id.buttonRandom)
+        val textNumber = findViewById<View>(R.id.textNumber)
+
+        buttonRandom.setOnClickListener(this)
+        textNumber.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View?) {
+      if(v?.id == R.id.buttonRandom || v?.id == R.id.textNumber) {
+          val id = random()
+      }
+    }
+
+    fun random(): Int {
+        return Random.nextInt(50) + 1
+    }
+}
